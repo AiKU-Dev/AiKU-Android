b1fe5cc4051ef74cfef0df242808f81151ef2d90
hodak <jlkim909@gmail.com>
2025-03-11 16:30:48 +0900

    build: build-logic and convention
    This commit introduces a `build-logic` directory for centralizing build logic and convention plugins.
    - Creates `build-logic/convention/build.gradle.kts` to define the convention plugin with dependencies on Android, Compose, Kotlin, KSP, and Room Gradle plugins.
    - Adds `build-logic/settings.gradle.kts` to include the convention plugin and configure version catalog usage with `libs.versions.toml`.
    - Includes the `build-logic` build in the main `settings.gradle.kts`.
    - Updates `libs.versions.toml` with new versions and dependencies, including `room`, `hilt` and their dependencies for build logic use, and separates plugin dependencies and versions from the original part, making them well-structured.
    - Uses `libs.versions.toml` to get type-safe accessors for dependencies and versions in the build logic.

---
+++ build-logic/convention/build.gradle.kts
@@ -0,0 +1,33 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    `kotlin-dsl`
+}
+
+group = "com.hyunjung.aiku.buildlogic"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget = JvmTarget.JVM_17
+    }
+}
+
+dependencies {
+    compileOnly(libs.android.gradlePlugin)
+    compileOnly(libs.compose.gradlePlugin)
+    compileOnly(libs.kotlin.gradlePlugin)
+    compileOnly(libs.ksp.gradlePlugin)
+    compileOnly(libs.room.gradlePlugin)
+}
+
+tasks {
+    validatePlugins {
+        enableStricterValidation = true
+        failOnWarning = true
+    }
+}
