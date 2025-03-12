f3c80f14f1f203e8b11317b24818b61af9ebdabe
최현정 <hjchoi.dev@gmail.com>
2025-03-12 16:29:08 +0900

    init: Add colors

--- core/designsystem/src/main/kotlin/com/hyunjung/aiku/core/designsystem/theme/Colors.kt
+++ core/designsystem/src/main/kotlin/com/hyunjung/aiku/core/designsystem/theme/Colors.kt
@@ -1,3 +1,60 @@
 package com.hyunjung.aiku.core.designsystem.theme
 
-// Todo : aiku colors
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+// Primary (Brand color)
+val CobaltBlue = Color(0xFF201CAF)
+
+// Green
+val Green = Color(0xFF76E2B5)
+val GreenLight = Color(0xFF8DE7C2)
+val GreenPale = Color(0xFFB0EED4)
+
+val Green01 = Color(0xFFF1FCF8)
+val Green02 = Color(0xFFC7F3E1)
+val Green03 = Color(0xFFB0EEF4)
+val Green04 = Color(0xFF8DE7C2)
+val Green05 = Color(0xFF76E2B5)
+
+// Purple
+val Purple01 = Color(0xFFFAFAFE)
+val Purple02 = Color(0xFFEAEAFD)
+val Purple03 = Color(0xFFE1E1FC)
+val Purple04 = Color(0xFFD5D4FA)
+val Purple05 = Color(0xFFCCCBF9)
+
+// Yellow
+val Yellow01 = Color(0xFFFFFDF3)
+val Yellow02 = Color(0xFFFEF6CF)
+val Yellow03 = Color(0xFFFDF3BB)
+val Yellow04 = Color(0xFFFDEE9D)
+val Yellow05 = Color(0xFFFCEA89)
+val AkuYellow = Color(0xFFF6D72F)
+
+// Gray
+val Gray00 = Color(0xFFB5B5BB)
+val Gray01 = Color(0xFFF6F6F6)
+val Gray02 = Color(0xFFDCDCDD)
+val Gray03 = Color(0xFF979798)
+val Gray04 = Color(0xFF565657)
+val Gray05 = Color(0xFF333335)
+val Gray06 = Color(0xFF171719)
+
+// Etc.
+val White = Color(0xFFFFFFFF)
+val Typo = Color(0xFF1E1927)
+val Red01 = Color(0xFFF66879)
+val Orange01 = Color(0xFFFF601C)
+val HurryRed = Color(0xFFFD1E1E)
+val LovePink = Color(0xFFFC2EAA)
+val SorryGreen = Color(0xFF08DE6A)
+val BabyPink = Color(0xFFFDE1E4)
+val SkyBlue = Color(0xFFA6A4DF)
