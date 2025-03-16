4e46f51af4e84daf90ea9d1d276481cf717562c7
hodak <jlkim909@gmail.com>
2025-03-17 00:58:03 +0900

    feat: Add presentation module

---
+++ presentation/build.gradle.kts
@@ -0,0 +1,8 @@
+plugins {
+    alias(libs.plugins.aiku.android.presentation)
+    alias(libs.plugins.aiku.android.library.compose)
+}
+
+android{
+    namespace = "com.hyunjung.aiku.presentation"
+}
