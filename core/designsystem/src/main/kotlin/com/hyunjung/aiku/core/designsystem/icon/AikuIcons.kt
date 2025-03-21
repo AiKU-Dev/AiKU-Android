0776187dc4462064192d4c2229a8e43846e44e41
hodak <jlkim909@gmail.com>
2025-03-21 12:42:04 +0900

    feat: Add Location icon to the design system

---
+++ core/designsystem/src/main/kotlin/com/hyunjung/aiku/core/designsystem/icon/AikuIcons.kt
@@ -0,0 +1,11 @@
+package com.hyunjung.aiku.core.designsystem.icon
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import com.hyunjung.aiku.core.designsystem.R
+
+object AikuIcons {
+    val Location: Painter
+        @Composable get() = painterResource(id = R.drawable.core_designsystem_ic_location)
+}
