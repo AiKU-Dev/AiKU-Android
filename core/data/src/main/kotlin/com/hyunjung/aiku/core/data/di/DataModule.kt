25c8b4a4c572d5c3b0557f8e686bec708dbff131
hodak <jlkim909@gmail.com>
2025-05-05 22:37:29 +0900

    feat: Add DataModule for dependency injection
    - Added `DataModule` to manage data layer dependencies.
    - Configured `GroupDataSource` binding to `DemoGroupDataSource`.
    - Configured `GroupRepository` binding to `DefaultGroupRepository`.

---
+++ core/data/src/main/kotlin/com/hyunjung/aiku/core/data/di/DataModule.kt
@@ -0,0 +1,25 @@
+package com.hyunjung.aiku.core.data.di
+
+import com.hyunjung.aiku.core.data.datasource.GroupDataSource
+import com.hyunjung.aiku.core.data.demo.DemoGroupDataSource
+import com.hyunjung.aiku.core.data.repository.DefaultGroupRepository
+import com.hyunjung.aiku.core.data.repository.GroupRepository
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class DataModule {
+
+    @Binds
+    internal abstract fun bindsGroupDataSource(
+        dataSource: DemoGroupDataSource
+    ): GroupDataSource
+
+    @Binds
+    internal abstract fun bindsGroupRepository(
+        groupRepositoryImpl: DefaultGroupRepository
+    ): GroupRepository
+}
