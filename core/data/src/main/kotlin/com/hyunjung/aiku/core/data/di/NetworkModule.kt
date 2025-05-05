0d232d70807b5e64a190711469865b70f1f2301c
hodak <jlkim909@gmail.com>
2025-05-05 22:30:54 +0900

    refactor: Rename DataModule to NetworkModule and Add Json Provider
    - Renamed `DataModule` to `NetworkModule` for better clarity of its purpose.
    - Added a `Json` provider to `NetworkModule` for network-related operations.
    - Configured `Json` to ignore unknown keys.

--- core/data/src/main/kotlin/com/hyunjung/aiku/core/data/di/NetworkModule.kt
+++ core/data/src/main/kotlin/com/hyunjung/aiku/core/data/di/NetworkModule.kt
@@ -7,11 +7,18 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import kotlinx.serialization.json.Json
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-internal object DataModule {
+internal object NetworkModule {
+    @Provides
+    @Singleton
+    fun providesNetworkJson(): Json = Json {
+        ignoreUnknownKeys = true
+    }
+
     @Provides
     @Singleton
     fun providesDemoAssetManager(
