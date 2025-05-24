bde4d643583315e43190a31fffb2bb9aa56f13ff
최현정 <hjchoi.dev@gmail.com>
2025-05-25 00:15:57 +0900

    feat: Add SplashActivity

--- presentation/src/main/kotlin/com/hyunjung/aiku/presentation/splash/SplashActivity.kt
+++ presentation/src/main/kotlin/com/hyunjung/aiku/presentation/splash/SplashActivity.kt
@@ -4,33 +4,21 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.core.LinearEasing
-import androidx.compose.animation.core.RepeatMode
-import androidx.compose.animation.core.animateFloat
-import androidx.compose.animation.core.infiniteRepeatable
-import androidx.compose.animation.core.rememberInfiniteTransition
-import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clipToBounds
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -64,19 +52,6 @@ class SplashActivity : ComponentActivity() {
 fun SplashScreen(
     onSplashFinished: () -> Unit
 ) {
-    // 애니메이션 상태
-    val infiniteTransition = rememberInfiniteTransition(label = "splash_anim")
-    val offsetX by infiniteTransition.animateFloat(
-        initialValue = -30f,
-        targetValue = 30f,
-        animationSpec = infiniteRepeatable(
-            animation = tween(500, easing = LinearEasing),
-            repeatMode = RepeatMode.Reverse
-        ),
-        label = "offsetX"
-    )
-
-    // 2초 후 콜백 실행
     LaunchedEffect(Unit) {
         delay(2000)
         onSplashFinished()
@@ -97,46 +72,37 @@ fun SplashScreen(
                 fontSize = 48.sp
             )
             SplashCharactersRow()
+
+
         }
     }
 }
 
 @Composable
 fun SplashCharactersRow() {
-    val screenWidth = LocalConfiguration.current.screenWidthDp.dp
-
-    Box(
+    Row(
         modifier = Modifier
-            .clipToBounds() // Box 영역 밖은 잘림
+            .wrapContentWidth(align = Alignment.CenterHorizontally, unbounded = true),
+        horizontalArrangement = Arrangement.spacedBy(16.dp),
+        verticalAlignment = Alignment.CenterVertically
     ) {
-        Row(
-            modifier = Modifier
-                .align(Alignment.Center)
-                .width(screenWidth + 120.dp)
-                .offset(x = (-60).dp), // 왼쪽으로 이동!
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Image(
-                painter = painterResource(id = R.drawable.presentation_char_1),
-                contentDescription = null,
-                modifier = Modifier.padding(horizontal = 8.dp)
+        Image(
+            painter = painterResource(id = R.drawable.presentation_char_1),
+            contentDescription = "Character 1",
+        )
+        Image(
+            painter = painterResource(id = R.drawable.presentation_char_2),
+            contentDescription = "Character 2",
+
             )
-            Image(
-                painter = painterResource(id = R.drawable.presentation_char_2),
-                contentDescription = null,
-                modifier = Modifier.padding(horizontal = 8.dp)
-            )
-            Image(
-                painter = painterResource(id = R.drawable.presentation_char_3),
-                contentDescription = null,
-                modifier = Modifier.padding(horizontal = 8.dp)
-            )
-            Image(
-                painter = painterResource(id = R.drawable.presentation_char_4),
-                contentDescription = null,
-                modifier = Modifier.padding(horizontal = 8.dp)
-            )
-        }
+        Image(
+            painter = painterResource(id = R.drawable.presentation_char_3),
+            contentDescription = "Character 3"
+        )
+        Image(
+            painter = painterResource(id = R.drawable.presentation_char_4),
+            contentDescription = "Character 4",
+        )
     }
 }
 
