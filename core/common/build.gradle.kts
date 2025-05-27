3304f4be9e020ecfb02a05a3e6408950b9e28217
hodak <jlkim909@gmail.com>
2025-05-27 11:05:27 +0900

    refactor: Simplify Android library configuration and enhance build logic

--- core/common/build.gradle.kts
+++ core/common/build.gradle.kts
@@ -3,6 +3,10 @@ plugins {
     alias(libs.plugins.aiku.android.hilt)
 }
 
+android {
+    namespace = "com.hyunjung.aiku.core.common"
+}
+
 dependencies {
     implementation(libs.kotlinx.coroutines.core)
 }
