97df6bed1b35f8f4151b3659d81fdbcb41b7a03d
hodak <jlkim909@gmail.com>
2025-06-24 01:46:28 +0900

    refactor: Rename DataSourceModule to KtorDataSourceModule

--- core/network/src/main/kotlin/com/hyunjung/aiku/core/network/di/KtorDataSourceModule.kt
+++ core/network/src/main/kotlin/com/hyunjung/aiku/core/network/di/KtorDataSourceModule.kt
@@ -9,7 +9,7 @@ import dagger.hilt.components.SingletonComponent
 
 @Module
 @InstallIn(SingletonComponent::class)
-internal interface DataSourceModule {
+internal interface KtorDataSourceModule {
 
     @Binds
     fun bindGroupDataSource(
