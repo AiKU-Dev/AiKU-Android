ddb6cca7f6f040c6a596cc80d854205bdaba424b
hodak <jlkim909@gmail.com>
2025-06-23 16:59:47 +0900

    chore: Update temporary token in BearerTokenManager

--- core/network/src/main/kotlin/com/hyunjung/aiku/core/network/auth/BearerTokenManager.kt
+++ core/network/src/main/kotlin/com/hyunjung/aiku/core/network/auth/BearerTokenManager.kt
@@ -8,8 +8,8 @@ import javax.inject.Singleton
 class BearerTokenManager @Inject constructor() {
     // todo : 임시 토큰 값 설정. KAKAO API 연동 후 변경할 것.
     var token: BearerTokens? = BearerTokens(
-        "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxIiwiYXV0aCI6Ik1FTUJFUiIsImV4cCI6MTc0OTQ2MDY0OH0.OfD3WGWPZvzjwJbanm0VF6isFmG3EVuCqQwXTUHefxQ",
-        ""
+        "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxIiwiYXV0aCI6Ik1FTUJFUiIsImV4cCI6MTc1MjczODk3MH0.PK6iGz7_3MgIf7SkAndmYQjTelU8t-26I_sLikTuXdg",
+        "eyJhbGciOiJIUzI1NiJ9.eyJleHAiOjE3NTI3Mzg5NzB9.JT1RgIctGnmEfyDcT7fasYt8oW-MwccafFvxH8l_uq8"
     )
         private set
 
