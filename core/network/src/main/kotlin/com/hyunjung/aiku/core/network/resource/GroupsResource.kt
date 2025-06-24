f0d83ae716bec1ca3dac31dd027790f54916abea
hodak <jlkim909@gmail.com>
2025-06-24 21:42:27 +0900

    feat: Define schedule-related API resources
    - MemberResource 생성 및 `/member/schedules` endpoint 정의
    - GroupsResource `/groups/{id}/schedules` endpoint 추가

--- core/network/src/main/kotlin/com/hyunjung/aiku/core/network/resource/GroupsResource.kt
+++ core/network/src/main/kotlin/com/hyunjung/aiku/core/network/resource/GroupsResource.kt
@@ -3,7 +3,15 @@ package com.hyunjung.aiku.core.network.resource
 import io.ktor.resources.Resource
 
 @Resource("/groups")
-class Groups(val page: Int? = null) {
+class Groups(val page: Int? = 1) {
     @Resource("{id}")
-    class Id(val parent: Groups = Groups(), val id: Long)
+    class Id(val parent: Groups = Groups(), val id: Long) {
+        @Resource("schedules")
+        class Schedules(
+            val parent: Id,
+            val page: Int? = 1,
+            val startDate: String? = null,
+            val endDate: String? = null
+        )
+    }
 }
