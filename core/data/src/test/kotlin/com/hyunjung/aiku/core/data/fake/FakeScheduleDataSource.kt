51203833f4d43a9c85a54a9c5c5b8147da97a0e2
hodak <jlkim909@gmail.com>
2025-06-25 18:14:58 +0900

    test: add FakeScheduleDataSource
    - getSchedules, getGroupSchedules에 대한 더미 데이터 반환

---
+++ core/data/src/test/kotlin/com/hyunjung/aiku/core/data/fake/FakeScheduleDataSource.kt
@@ -0,0 +1,53 @@
+package com.hyunjung.aiku.core.data.fake
+
+import com.hyunjung.aiku.core.data.datasource.ScheduleDataSource
+import com.hyunjung.aiku.core.data.model.GroupSchedule
+import com.hyunjung.aiku.core.data.model.Location
+import com.hyunjung.aiku.core.data.model.Schedule
+import com.hyunjung.aiku.core.data.model.ScheduleStatus
+import java.time.LocalDateTime
+
+class FakeScheduleDataSource : ScheduleDataSource {
+
+    override suspend fun getSchedules(
+        page: Int,
+        startDate: LocalDateTime?,
+        endDate: LocalDateTime?
+    ): List<Schedule> = listOf(
+        Schedule(
+            groupId = 1L,
+            groupName = "건국대학교",
+            scheduleId = 6L,
+            scheduleName = "운영체제",
+            location = Location(
+                latitude = 127.1,
+                longitude = 111.1,
+                locationName = "공학관"
+            ),
+            scheduleTime = LocalDateTime.parse("2024-08-20T12:12:12"),
+            memberSize = 10,
+            scheduleStatus = ScheduleStatus.RUNNING
+        )
+    )
+
+    override suspend fun getGroupSchedules(
+        groupId: Long,
+        page: Int,
+        startDate: LocalDateTime?,
+        endDate: LocalDateTime?
+    ): List<GroupSchedule> = listOf(
+        GroupSchedule(
+            scheduleId = 6L,
+            scheduleName = "운영체제",
+            location = Location(
+                latitude = 127.1,
+                longitude = 111.1,
+                locationName = "공학관"
+            ),
+            scheduleTime = LocalDateTime.parse("2024-08-20T12:12:12"),
+            scheduleStatus = ScheduleStatus.RUNNING,
+            memberSize = 10,
+            accept = true
+        )
+    )
+}
