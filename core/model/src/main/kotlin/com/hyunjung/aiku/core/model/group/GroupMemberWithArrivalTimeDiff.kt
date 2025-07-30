c700c8ca3c28f15d980594a1e315692557cab63b
hodak <jlkim909@gmail.com>
2025-07-30 23:23:49 +0900

    feat: Restructure GroupMember hierarchy with base class
    - `GroupMemberBase` 추상 클래스를 도입하여 공통 프로퍼티(`id`, `nickname`, `memberProfileImage`)를 정의했습니다.
    - 다양한 맥락별 멤버 모델(`GroupMember`, `GroupMemberWithPoint`, `GroupMemberWithArrivalTimeDiff`)이 이를 상속하도록 구성해, 중복된 필드 선언을 제거하고 타입 간 일관성과 다형성을 확보했습니다.

--- core/model/src/main/kotlin/com/hyunjung/aiku/core/model/group/GroupMemberWithArrivalTimeDiff.kt
+++ core/model/src/main/kotlin/com/hyunjung/aiku/core/model/group/GroupMemberWithArrivalTimeDiff.kt
@@ -2,8 +2,9 @@ package com.hyunjung.aiku.core.model.group
 
 import com.hyunjung.aiku.core.model.profile.MemberProfileImage
 
-data class GroupMember(
+data class GroupMemberWithArrivalTimeDiff(
     override val id: Long,
     override val nickname: String,
-    override val memberProfileImage: MemberProfileImage
+    override val memberProfileImage: MemberProfileImage,
+    val arrivalTimeDiffMinutes: Int
 ) : GroupMemberBase(id, nickname, memberProfileImage)
--- core/model/src/main/kotlin/com/hyunjung/aiku/core/model/group/GroupMemberWithPoint.kt
+++ core/model/src/main/kotlin/com/hyunjung/aiku/core/model/group/GroupMemberWithPoint.kt
@@ -2,8 +2,9 @@ package com.hyunjung.aiku.core.model.group
 
 import com.hyunjung.aiku.core.model.profile.MemberProfileImage
 
-data class GroupMember(
+data class GroupMemberWithPoint(
     override val id: Long,
     override val nickname: String,
-    override val memberProfileImage: MemberProfileImage
+    override val memberProfileImage: MemberProfileImage,
+    val point: Int
 ) : GroupMemberBase(id, nickname, memberProfileImage)
